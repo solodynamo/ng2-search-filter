169e77ac284a280637ef9a2e164f5ff6d71cfa76
solodynamo <bittuf3@gmail.com>
2017-06-04 09:40:06 +0530

    Modifies tsconfig and adds metadata for module resolution

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './src/ng2-filter.module';
-export * from './src/ng2-filter.pipe';
--- dist/src/ng2-filter.pipe.d.ts
+++
@@ -1,8 +0,0 @@
-import { PipeTransform } from "@angular/core";
-export declare class Ng2SearchPipe implements PipeTransform {
-    /**
-     * @items = object from array
-     * @term = term's search
-     */
-    transform(items: any, term: any): any;
-}
