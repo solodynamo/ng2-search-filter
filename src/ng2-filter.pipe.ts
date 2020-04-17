52317aa397b349722a1b82dc212070e5710e618c
Jose Gracia Berenguer <jgracia9988@gmail.com>
2020-04-17 16:24:28 +0200

    added accent/tilde support

--- src/ng2-filter.pipe.ts
+++ src/ng2-filter.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform, Injectable } from "@angular/core";
+import deburr from 'lodash.deburr';
 
 @Pipe({
   name: 'filter',
@@ -8,51 +9,48 @@ import { Pipe, PipeTransform, Injectable } from "@angular/core";
 export class Ng2SearchPipe implements PipeTransform {
 
   /**
-     * @param items object from array
-     * @param term term's search
-     * @param excludes array of strings which will ignored during search
-     */
-  transform(items: any, term: string, excludes: any = []): any {
-    if (!term || !items) return items;
-
-    return Ng2SearchPipe.filter(items, term, excludes);
-  }
-
-  /**
-   *
    * @param items List of items to filter
    * @param term  a string term to compare with every property of the list
    * @param excludes List of keys which will be ignored during search
-   *
    */
   static filter(items: Array<{ [key: string]: any }>, term: string, excludes: any): Array<{ [key: string]: any }> {
 
-    const toCompare = term.toLowerCase();
+    const toCompare = deburr(term.toLowerCase());
 
+    // tslint:disable-next-line:no-shadowed-variable
     function checkInside(item: any, term: string) {
-      
-      if (typeof item === "string" && item.toString().toLowerCase().includes(toCompare)) {
+
+      if (typeof item === 'string' && deburr(item.toString().toLowerCase()).includes(toCompare)) {
         return true;
       }
 
-      for (let property in item) {
-        if (item[property] === null || item[property] == undefined || excludes.includes(property)) {
+      for (const property in item) {
+        if (item[property] === null || item[property] === undefined || excludes.includes(property)) {
           continue;
         }
         if (typeof item[property] === 'object') {
           if (checkInside(item[property], term)) {
             return true;
           }
-        }
-        else if (item[property].toString().toLowerCase().includes(toCompare)) {
+        } else if (deburr(item[property].toString().toLowerCase()).includes(toCompare)) {
           return true;
         }
       }
       return false;
     }
 
-    return items.filter(function (item) {
-      return checkInside(item, term);
-    });
+    return items.filter(item => checkInside(item, term));
+  }
+  /**
+   * @param items object from array
+   * @param term term's search
+   * @param excludes array of strings which will ignored during search
+   */
+  transform(items: any, term: string, excludes: any = []): any {
+    if (!term || !items) {
+      return items;
+    }
+
+    return Ng2SearchPipe.filter(items, term, excludes);
   }
 }
